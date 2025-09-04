093c2f33f668ee984ab7dc37443b77291f32b188
williammelomel7-debug <williammelomel7@gmail.com>
2025-09-04 19:37:42 -0300

    Add files via upload

---
+++ ds1-cp/js/opcyvj3640903.js
@@ -0,0 +1,30 @@
+
+      function atomiShowItems({items}) {
+        items.forEach((item) => {
+          const hiddenItem = [...document.querySelectorAll(`#${item}`), ...document.querySelectorAll(`.${item}`)];
+          console.log("hiddenItem", hiddenItem)
+          if (hiddenItem?.length > 0) {
+            hiddenItem.forEach(item => item.classList.remove("atomicat-delay"));
+          }
+        })
+      }
+    (function() {
+          try {
+              const clickeventList = [{"compKey":"f39fc06","misc":{"type":"button"}},{"compKey":"2b7840d","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
